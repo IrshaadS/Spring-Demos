98b1b6f67335e4ffc9f4f7539e366721c0e6b04f
IrshaadS <Irshaad_S@rediffmail.com>
2018-07-12 20:11:03 +0530

    LDAP Demo

---
+++ LDAP-Authentication-Demo/src/main/java/main/java/hello/LdapAuthenticationDemoApplication.java
@@ -0,0 +1,12 @@
+package main.java.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdapAuthenticationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LdapAuthenticationDemoApplication.class, args);
+	}
+}
